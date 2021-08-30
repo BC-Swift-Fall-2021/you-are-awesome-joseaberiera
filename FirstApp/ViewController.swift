9958925f1a072d898f7cd7ad13be1aaf3223488c
Jose Riera <rieraja@bc.edu>
2021-08-30 18:45:01 -0400

    first class You Are Awesome app

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("😎 viewDidLoad has executed!")
     }
 
 
+    @IBAction func sayItButtonPressed(_ sender: UIButton) {
+        print("🤩 sayIt was pressed")
+        messageLabel.text = "You Are Awesome!"
+    }
 }
 
