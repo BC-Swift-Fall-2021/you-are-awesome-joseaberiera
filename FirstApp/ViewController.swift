8627d208870c723ce2d2ebaad02cf0037e72fe67
Jose Riera <rieraja@bc.edu>
2021-09-12 17:02:06 -0400

    2.5-2.8 (I just realized I forgot to commit last week so I'm going back and doing them again)

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -9,18 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = ""
         // Do any additional setup after loading the view.
         
-        print("😎 viewDidLoad has executed!")
     }
 
 
     @IBAction func sayItButtonPressed(_ sender: UIButton) {
-        print("🤩 sayIt was pressed")
-        messageLabel.text = "You Are Awesome!"
+        let awesomeMessage = "You Are Awesome!"
+        let greatMessage = "You Are Great!"
+        let bombMessage = "You Are Da Bomb!"
+        
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        }else if messageLabel.text == greatMessage{
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        }else{
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
+        
+        
     }
 }
 
